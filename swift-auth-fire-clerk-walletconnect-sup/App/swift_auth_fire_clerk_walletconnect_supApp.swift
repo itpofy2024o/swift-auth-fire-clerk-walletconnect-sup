8cf540ec5f0af8c3f37480ca5e0d91eff20f7125
itpofy2024o <qweasdzxcf1G@proton.me>
2024-08-29 01:39:15 +0800

    welcome fcuker

--- swift-auth-fire-clerk-walletconnect-sup/App/swift_auth_fire_clerk_walletconnect_supApp.swift
+++ swift-auth-fire-clerk-walletconnect-sup/App/swift_auth_fire_clerk_walletconnect_supApp.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 @main
 struct swift_auth_fire_clerk_walletconnect_supApp: App {
+    @StateObject var fireAuthModel = AuthFirebaseViewModel()
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environmentObject(fireAuthModel)
         }
     }
 }
