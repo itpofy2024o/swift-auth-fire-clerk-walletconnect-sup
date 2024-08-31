c976994debedaf14f2c33d383b3f9b0fe7e12705
itpofy2024o <qweasdzxcf1G@proton.me>
2024-08-31 23:03:42 +0800

    auto return to login screen after sending password reset

--- swift-auth-fire-clerk-walletconnect-sup/Core/Auth/Views/ForgetPasswordView.swift
+++ swift-auth-fire-clerk-walletconnect-sup/Core/Auth/Views/ForgetPasswordView.swift
@@ -14,32 +14,46 @@ struct ForgetPasswordView: View {
     @EnvironmentObject var authViewModel: AuthFirebaseViewModel
 
     var body: some View {
-        VStack {KFImage(URL(string:"https://moralis.io/wp-content/uploads/2023/11/Phantom-Wallet.png"))
-                .resizable()
-                .scaledToFit()
-                .frame(width:UIScreen.main.bounds.width*0.4)
-                .cornerRadius(25)
-                .padding(.vertical,26)
-            
-            AuthenticationInputView(text:$email,label: "Email Address",placeholder: "youremailaddress@smtp.com")
-                .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
-                .padding(.horizontal,28)
-                .padding(.top,UIScreen.main.bounds.width*0.05)
-            
-            Button("Send Password Reset Link") {
-                Task {
-                    await authViewModel.resetPassword(forEmail: email)
-                    isResetLinkSent = true
+        NavigationStack {
+            VStack {KFImage(URL(string:"https://moralis.io/wp-content/uploads/2023/11/Phantom-Wallet.png"))
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:UIScreen.main.bounds.width*0.4)
+                    .cornerRadius(25)
+                    .padding(.vertical,26)
+                
+                AuthenticationInputView(text:$email,label: "Email Address",placeholder: "youremailaddress@smtp.com")
+                    .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+                    .padding(.horizontal,28)
+                    .padding(.top,UIScreen.main.bounds.width*0.05)
+                
+                Button("Send Password Reset Link") {
+                    Task {
+                        await authViewModel.resetPassword(forEmail: email)
+                        isResetLinkSent = true
+                    }
+                }
+                .padding()
+                
+                if isResetLinkSent {
+                    Text("Succeeded!!")
+                        .font(.subheadline)
+                        .foregroundColor(.green)
+                    Text("Should the provided email was registed, a reset link is await in your email.")
+                        .foregroundColor(.orange)
+                        .font(.caption)
                 }
             }
             .padding()
-            
-            if isResetLinkSent {
-                Text("A password reset link has been sent to your email.")
-                    .foregroundColor(.green)
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                    return
+                }
+            }.navigationDestination(isPresented: $isResetLinkSent) {
+                AuthView(method: "firebase")
+                    .navigationBarBackButtonHidden(true)
             }
         }
-        .padding()
     }
 }
 
