a36bacb2c0271931e8c84e995bf05d646c026484
itpofy2024o <qweasdzxcf1G@proton.me>
2024-09-08 15:16:02 +0800

    may try clerk when sdk became alpha

--- swift-auth-fire-clerk-walletconnect-sup/Core/Auth/ViewModels/ClerkViewModel.swift
+++ swift-auth-fire-clerk-walletconnect-sup/Core/Auth/ViewModels/ClerkViewModel.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import SwiftUI
+import FirebaseFirestore
+import ClerkSDK
+import Combine
+
+class ClerkViewModel: ObservableObject {
+    @ObservedObject private var clerk = Clerk.shared
+    @Published var currentUser: UserFirebase?
+    @Published var authStatus: AuthStatus = .unknown
+    @Published var email: String = ""
+    @Published var otpCode: String = ""
+    @Published var fullname: String = ""
+    @Published var username: String = ""
+    
+    enum AuthStatus {
+        case loggedIn
+        case loggedOut
+        case unknown
+    }
+    
+    func authenticateUser() {
+    }
+}
